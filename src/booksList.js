c5617be796ffa20fcd0dbfcc8764b5a7c0e063f1
Valeria Toti <valeria.toti@gmail.com>
2018-09-25 18:36:48 +0200

    modifiacati i link epub

--- src/booksList.js
+++ src/booksList.js
@@ -7,13 +7,13 @@ module.exports = {
       image: img1,
       alt: 'Copertina dell\'ebook: Cluetrian Manifesto',
       description: 'Le nuove tesi del Cluetrain Manifesto',
-      download: `https://github.com/valeriatoti/ebook-accessibili/blob/master/src/download/CluetrianManifesto.epub`
+      download: `https://github.com/valeriatoti/ebook-accessibili/blob/master/src/download/CluetrianManifesto.epub?raw=true`
      },
      book2: {
       title: 'Aaron Swartz — Una vita per la cultura libera e la giustizia sociale',
       image: img2,
       alt: 'Copertina dell\'ebook: Aaron Swartz',
       description: 'Aaron Swartz — Una vita per la cultura libera e la giustizia sociale',
-      download: `https://github.com/valeriatoti/ebook-accessibili/blob/master/src/download/AARON.epub`
+      download: `https://github.com/valeriatoti/ebook-accessibili/blob/master/src/download/AARON.epub?raw=true`
      }
 }
