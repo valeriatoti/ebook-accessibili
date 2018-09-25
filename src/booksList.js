deb1fb08ea97113944c6cb745b40b9be00d70569
Valeria Toti <valeria.toti@gmail.com>
2018-09-25 11:39:49 +0200

    modificato percorso img

--- src/booksList.js
+++ src/booksList.js
@@ -1,16 +1,17 @@
+
 module.exports = {
    book1: {
       title: 'Clue train manifesto',
-      image: './images/cover-new-clues.jpg',
+      image: '../images/cover-new-clues.jpg',
       alt: 'Copertina dell\'ebook: Cluetrian Manifesto',
       description: 'Le nuove tesi del Cluetrain Manifesto',
-      download: './download/CluetrianManifesto.epub'
+      download: '../download/CluetrianManifesto.epub'
      },
      book2: {
       title: 'Aaron Swartz — Una vita per la cultura libera e la giustizia sociale',
-      image: './images/cover-aaron.jpg',
+      image: '../images/cover-aaron.jpg',
       alt: 'Copertina dell\'ebook: Aaron Swartz',
       description: 'Aaron Swartz — Una vita per la cultura libera e la giustizia sociale',
-      download: './download/CluetrianManifesto.epub'
+      download: '../download/CluetrianManifesto.epub'
      }
 }
