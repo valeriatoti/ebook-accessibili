06c840e412e2b4b9497ee5bbb3dbb5453ae8ab92
Valeria Toti <valeria.toti@gmail.com>
2018-12-09 15:54:17 +0100

    update contact form

--- src/pages/contatti.js
+++ src/pages/contatti.js
@@ -29,27 +29,18 @@ const ContactPage = () => (
                 </button>
             </p>
         </form> */}
+    <form
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input name="name" placeholder="Your Name" type="text" />
+      <input name="email" placeholder="name@name.com" type="email" />
+      <textarea name="message" />
+      <button>Send</button>
+    </form>
 
-        <form action="/" name="contact" method="post" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
 
     </Layout>
 )
