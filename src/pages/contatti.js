48470b82326d4fab029f99c2661465c7187c9475
Valeria Toti <valeria.toti@gmail.com>
2018-12-09 15:36:26 +0100

    update contact form

--- src/pages/contatti.js
+++ src/pages/contatti.js
@@ -7,7 +7,7 @@ const ContactPage = () => (
         <h1>Ciao, mi chiamo Valeria Toti <span role="img" aria-label="mano che saluta">👋</span></h1>
         <p className="big-text">Hai una domanda per me?<br/> Vuoi che trasformi il tuo libro in un ebook ben impaginato e accessibile?<br/> Scrivimi, sarò felice di aiutarti se posso.<br/> Qui c'è il form per mandarmi il tuo messaggio:</p>
 
-        <form name="form-contact" method="post" id="contact" data-netlify="true">
+        {/* <form name="form-contact" method="post" id="contact" data-netlify="true">
             <p>
                 <label for="name">Il tuo nome
                 <input type="text" name="name" id="name" placeholder="Nome" required  aria-required="true" autocomplete="name"/>
@@ -28,7 +28,28 @@ const ContactPage = () => (
                 INVIA
                 </button>
             </p>
-        </form>
+        </form> */}
+
+        <form name="contact" method="POST" netlify>
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
 
     </Layout>
 )
