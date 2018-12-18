4f85c1fc3d481e4b38a43b30f6b179290335afe1
Valeria Toti <valeria.toti@gmail.com>
2018-12-18 16:18:14 +0100

    update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,7 +28,7 @@ const Layout = ({ children }) => (
           ]}
         >
           <html lang="it" />
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous"></link>
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous"/>
         </Helmet>
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
