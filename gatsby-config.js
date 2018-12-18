20f8ca37d9486fc03a1b484713880d3233e6ebb3
Valeria Toti <valeria.toti@gmail.com>
2018-12-18 16:17:26 +0100

    update layout.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
       options: {
         trackingId: "UA-130687964-1",
         // Puts tracking script in the head instead of the body
-        head: false,
+        head: true,
         // Setting this parameter is optional
         anonymize: true,
         // Setting this parameter is also optional
