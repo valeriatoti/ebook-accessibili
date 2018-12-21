aacab85326ff522c6647edd69088aa8f5eec3d12
Valeria Toti <valeria.toti@gmail.com>
2018-12-21 08:01:41 +0100

    update favicon + manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,13 +7,14 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Libri digitali accessibili',
+        short_name: 'ebook accessibili',
+        lang: 'it',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#155F6F',
+        theme_color: '#155F6F',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     {
