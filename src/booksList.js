69eebe0fecb0363206a969f6a5a453fc5ecde598
Valeria Toti <valeria.toti@gmail.com>
2019-01-16 13:23:30 +0100

    add epub tao te ching

--- src/booksList.js
+++ src/booksList.js
@@ -1,14 +1,22 @@
 const img1 = require ('./images/steiner.jpg')
 const img2 = require ('./images/cover-aaron.jpg')
 const img3 = require ('./images/cover-new-clues.jpg')
+const img4 = require ('./images/cover-tao-te-ching.jpg')
 
 
 module.exports = {
+   book4: {
+      title: 'Tao Te Ching',
+      image: img4,
+      alt: 'Copertina dell\'ebook: Tao Te Ching',
+      description: 'Tao Te Ching — Il grande libro della Via e della Virtù • di Lao Tzu',
+      download: `https://github.com/valeriatoti/ebook-accessibili/blob/master/src/download/tao-te-ching.epub?raw=true`
+     },
    book3: {
       title: 'Clue train manifesto',
       image: img3,
       alt: 'Copertina dell\'ebook: Cluetrian Manifesto',
-      description: 'Le nuove tesi del Cluetrain Manifesto',
+      description: 'Le nuove tesi del Cluetrain Manifesto • di Doc Searls & David Weinberger',
       download: `https://github.com/valeriatoti/ebook-accessibili/blob/master/src/download/CluetrainManifesto.epub?raw=true`
      },
      book2: {
@@ -22,7 +30,7 @@ module.exports = {
         title: 'Il mondo dei Sensi e il mondo dello Spirito',
         image: img1,
         alt: 'Copertina dell\'ebook: Il mondo dei Sensi e il mondo dello Spirito di Rudolf Steiner',
-        description: 'Il mondo dei Sensi e il mondo dello Spirito',
+        description: 'Il mondo dei Sensi e il mondo dello Spirito • di Rudolf Steiner',
         download: `https://github.com/valeriatoti/ebook-accessibili/blob/master/src/download/sensi-e-spirito.epub?raw=true`
      }
 }
