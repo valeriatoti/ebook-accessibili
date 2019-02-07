340418fd941189e84ca9f0a35f7dd26f1da13bbe
Valeria Toti <valeria.toti@gmail.com>
2019-02-07 11:50:02 +0100

    added page about

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ const IndexPage = () => (
       <h1>Libri digitali accessibili</h1>
       <blockquote className="quote-image-cite">
         Gli ebook che trovi su questo sito sono creati con cura, rispettando le <em><a href="https://www.w3.org/Submission/2017/SUBM-epub-a11y-20170125/" rel="nofollow">specifiche dell'accessibilità</a></em>, in modo che tutti, anche le persone disabili, possano accedere al contenuto. Gli ebook sono gratuiti e senza <abbr title="Digital Rights Management: gestione dei diritti digitali." className="small-caps">DRM</abbr>.
-        <cite><a href="https://github.com/valeriatoti" rel="nofollow">
-      valeria toti</a><br/>
+        <cite><Link to="/about/">
+      valeria toti</Link><br/>
       ebook developer
       </cite>
       </blockquote>
