47f848c07ef51ee74c69c24b4b176a13f6691656
Valeria Toti <valeria.toti@gmail.com>
2019-02-15 22:01:15 +0100

    inserito styled component per css subnav

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,27 @@
 import React from 'react'
 import Navbar from './navbar/Navbar'
+import styled from 'styled-components'
+
+const SubNav = styled.div`
+  background: #F6F2EE;
+  margin: 0 auto;
+  padding: 0.8rem 1.0875rem;
+  text-align: right;
+  p {
+    margin: 0;
+    color: #5C4A52;
+  }
+`
 
 const Header = () => (
   <header>
-  <Navbar />
-    <div
-     style={{
-      background: '#F6F2EE',
-      margin: '0 auto',
-      padding: '0.8rem 1.0875rem',
-      textAlign: 'right',
-      }}
-      >
-      <p
-        style={{
-         margin: '0',
-         color: '#5C4A52',
-      }}
-      >
+    <Navbar />
+    <SubNav>
+      <p>
       Aiutami a creare ePub free e accessibili
       <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=6QAG82BXDSDLJ" role="button" className="donate">Fai una donazione</a>
       </p>
-    </div>
+    </SubNav>
   </header>
 )
 
